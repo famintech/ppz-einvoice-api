261c3d72e9ad00e884d7cd4ae2900e56feee708a
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-11-07 18:32:40 +0800

    fix routes

--- app/Modules/SPA/eInvoice/routes/api.php
+++ app/Modules/SPA/eInvoice/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Modules\SPA\eInvoice\Controllers\LoginTaxpayerController;
 
-Route::prefix('spa/einvoice')->group(function () {
+Route::prefix('/spa/einvoice')->group(function () {
     Route::get('/login-taxpayer', [LoginTaxpayerController::class, 'index']);
 });
