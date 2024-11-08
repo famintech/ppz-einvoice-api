3d3ed3b0f54e16b74598a8e003f01a9e10875429
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-11-08 09:19:25 +0800

    fix 404 routes

--- app/Modules/SPA/eInvoice/routes/api.php
+++ app/Modules/SPA/eInvoice/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Modules\SPA\eInvoice\Controllers\LoginTaxpayerController;
+use App\Modules\SPA\eInvoice\Http\Controllers\LoginTaxpayerController;
 
 Route::prefix('spa/einvoice')->group(function () {
     Route::get('/login-taxpayer', [LoginTaxpayerController::class, 'index']);
