65e8ace8df63a2fd0986083b22a54d759c61072d
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-11-08 16:11:55 +0800

    implement validate tin endpoint

--- app/Modules/SPA/eInvoice/Http/Controllers/eInvoicing/ValidateTINTaxpayerController.php
+++ app/Modules/SPA/eInvoice/Http/Controllers/eInvoicing/ValidateTINTaxpayerController.php
@@ -3,7 +3,28 @@
 namespace App\Modules\SPA\eInvoice\Http\Controllers\eInvoicing;
 
 use App\Modules\SPA\eInvoice\Http\Controllers\BaseApiController;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ValidateTINTaxpayerController extends BaseApiController
 {
+    private const ALLOWED_ID_TYPES = ['NRIC', 'PASSPORT', 'BRN', 'ARMY'];
+
+    public function __invoke(Request $request)
+    {
+        $validated = $request->validate([
+            'tin' => 'required|string',
+            'idType' => ['required', Rule::in(self::ALLOWED_ID_TYPES)],
+            'idValue' => 'required|string',
+        ]);
+
+        return $this->makeRequest(
+            'GET',
+            "/api/v1.0/taxpayer/validate/{$validated['tin']}",
+            [
+                'idType' => $validated['idType'],
+                'idValue' => $validated['idValue'],
+            ]
+        );
+    }
 }
--- app/Modules/SPA/eInvoice/routes/api.php
+++ app/Modules/SPA/eInvoice/routes/api.php
@@ -6,11 +6,17 @@ use App\Modules\SPA\eInvoice\Http\Controllers\Platform\GetAllDocumentTypesContro
 use App\Modules\SPA\eInvoice\Http\Controllers\Platform\GetDocumentTypeController;
 use App\Modules\SPA\eInvoice\Http\Controllers\Platform\GetDocumentTypeVersionController;
 use App\Modules\SPA\eInvoice\Http\Controllers\Platform\GetNotificationsController;
+use App\Modules\SPA\eInvoice\Http\Controllers\eInvoicing\ValidateTINTaxpayerController;
 
-Route::prefix('spa/einvoice')->group(function () {
+Route::prefix('spa/einvoice/lhdn/platform')->group(function () {
     Route::get('/login-taxpayer', LoginTaxpayerController::class);
     Route::get('/document-types', GetAllDocumentTypesController::class);
     Route::get('/document-type/{id}', GetDocumentTypeController::class);
     Route::get('/document-type/{id}/version/{vid}', GetDocumentTypeVersionController::class);
     Route::get('/notifications/taxpayer', GetNotificationsController::class);
 });
+
+Route::prefix('spa/einvoice/lhdn/e-invoicing')->group(function () {
+    Route::get('/validate-tin-taxpayer/{tin}', ValidateTINTaxpayerController::class);
+});
+
