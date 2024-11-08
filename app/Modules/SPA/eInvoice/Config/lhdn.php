d3b027e9f773a95dc33c08bf9fedf556f8fe71a2
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-11-08 10:34:20 +0800

    add LHDN config

---
+++ app/Modules/SPA/eInvoice/Config/lhdn.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Modules\SPA\Config;
+
+class LHDN
+{
+    public static function getApiBaseUrl()
+    {
+        return env('LHDN_API_BASE_URL', 'https://api.myinvois.hasil.gov.my');
+    }
+
+    public static function getIdSrvBaseUrl()
+    {
+        return env('LHDN_ID_SRV_BASE_URL', 'https://api.myinvois.hasil.gov.my');
+    }
+}
--- app/Modules/SPA/eInvoice/Http/Controllers/LoginTaxpayerController.php
+++ app/Modules/SPA/eInvoice/Http/Controllers/LoginTaxpayerController.php
@@ -1,13 +1,23 @@
 <?php
 
 namespace App\Modules\SPA\eInvoice\Http\Controllers;
+
 use App\Http\Controllers\Controller;
+use App\Modules\SPA\Config\LHDN;
 
 class LoginTaxpayerController extends Controller
 {
+    private $baseUrl;
+
+    public function __construct()
+    {
+        $this->baseUrl = LHDN::getApiBaseUrl();
+    }
+
     public function index()
     {
         return response()->json([
+            'base_url' => $this->baseUrl,
             'message' => 'API v1 is working'
         ]);
     }
