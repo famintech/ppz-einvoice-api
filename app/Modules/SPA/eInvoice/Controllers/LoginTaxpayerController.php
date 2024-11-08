2ffd4bd141d5c9b58610a54329bceb61376375d9
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-11-08 09:24:02 +0800

    fix 404 routes

--- app/Modules/SPA/eInvoice/Controllers/LoginTaxpayerController.php
+++ app/Modules/SPA/eInvoice/Controllers/LoginTaxpayerController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Modules\SPA\eInvoice\Http\Controllers;
+namespace App\Modules\SPA\eInvoice\Controllers;
 use App\Http\Controllers\Controller;
 
 class LoginTaxpayerController extends Controller
